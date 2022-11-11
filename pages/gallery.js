6d8680c8143f348aa20423f4e93d1965f98bfb6d
Andrew Iglinski <Andrew@igloodevelopment.dev>
2022-11-11 11:19:09 -0600

    another transition tweak

---
+++ pages/gallery.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/gallery.module.scss";
+
+const Gallery = () => {
+	return (
+		<div className={styles.galleryContainer}>
+			An image gallery, reviews or an online ordering page could go here...
+		</div>
+	);
+};
+
+export default Gallery;
